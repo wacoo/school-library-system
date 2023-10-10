ee014f886a99b5a8b32ae6fcd57fd8b3c5d54d86
wac <wabaham9@gmail.com>
2023-10-10 15:54:29 +0300

    Add decoration

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
 require('securerandom')
-class Person
+require_relative('nameable')
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -8,12 +10,17 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services
     return true if of_age || @parent_permission
 
     false
+  end
+
+  def correct_name
+    @name
   end
 
   private
